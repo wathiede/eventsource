c4e0a2d192e9d452faf7ed3ca59a080852167bae
Bill Thiede <git@xinu.tv>
2012-12-08 21:53:34 -0800

    Add X-Accel-Buffering header so SSE will work behind nginx.

--- http/consumer.go
+++ http/consumer.go
@@ -1,21 +1,25 @@
 package http
 
 import (
-    "net"
-    "net/http"
+	"net"
+	"net/http"
 )
 
 type consumer struct {
-    conn  net.Conn
-    close chan bool
+	conn  net.Conn
+	close chan bool
 }
 
 func newConsumer(resp http.ResponseWriter) (*consumer, error) {
-    conn, _, err := resp.(http.Hijacker).Hijack()
-    if err != nil {
-        return nil, err
-    }
-    conn.Write([]byte("HTTP/1.1 200 OK\nContent-Type: text/event-stream\n\n"))
+	conn, _, err := resp.(http.Hijacker).Hijack()
+	if err != nil {
+		return nil, err
+	}
+	conn.Write([]byte(`HTTP/1.1 200 OK
+Content-Type: text/event-stream
+X-Accel-Buffering: no
 
-    return &consumer{conn, make(chan bool)}, nil
+`))
+
+	return &consumer{conn, make(chan bool)}, nil
 }
